caf9903c5dfbf01d8b1833f863b705a19f6d9eef
Nilay Shrimanwar <shrimanwar92@gmail.com>
2016-11-29 18:05:45 +0530

    update url

--- src/app/countries.service.ts
+++ src/app/countries.service.ts
@@ -11,7 +11,7 @@ export class CountryService {
   constructor (private http: Http) {}
 
   getCountries(): Observable<any[]> {
-    return this.http.get('http://localhost:8080/countries')
+    return this.http.get('https://restcountries.eu/rest/v1')
                     .map(this.extractData)
                     .catch(this.handleError);
   }
